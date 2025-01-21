95e37a0e67f68ac67e09780f4369cc4e8cd33902
arjunv <arjunv.nxtgen.com>
2025-01-21 17:45:15 +0530

    added routes and endpoints

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -5,7 +5,28 @@ import db from "../db";
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {});
+router.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  //encrypt the password
+  const hacked = bcrypt.hashSync(password, 8);
+
+  // save the user and update ie to insert the value to the
+  const update_user = db.prepare(`INSERT into user(user,password) VALUES(?,?)`);
+  const result = update_user.run(username, hacked);
+
+  // after register they need to add their todo tasks
+  const todo = `Add your Todo`;
+  const add_todo = db.prepare(`INSERT into todos(user_id,task) VALUES(?,?)`);
+  add_todo.run(result.lastInsertRowid, todo);
+
+  //create a token
+  const token = jwt.sign(
+    { id: result, lastInsertRowid },
+    process.env.JWT_SECRET,
+    { expiresIn: "24h" }
+  );
+});
 
 router.post("/login", (req, res) => {});
 
